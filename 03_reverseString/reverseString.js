ed81ac9dc2327d172cb252acab0965ed1da2760d
rodolfo najera <rodolfonajera401@gmail.com>
2024-02-16 01:32:36 -0800

    Complete exercise 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+   let reversedString= '';
+   for(let i = word.length-1; i>=0; i--){
+    reversedString += word[i];
+   }
+   return reversedString;
 };
 
 // Do not edit below this line
