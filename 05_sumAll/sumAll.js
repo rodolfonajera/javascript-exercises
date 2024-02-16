e1f56788369602c826ee361bfde21a394642ded5
rodolfo najera <rodolfonajera401@gmail.com>
2024-02-16 03:25:09 -0800

    Complete exercise 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(firstNum, secondNum) {
+    let smallerNum = 0;
+    let largerNum = 0;
+    let value = 0;
 
+    if(firstNum<secondNum){
+        smallerNum = firstNum;
+        largerNum = secondNum;
+    }
+    else{
+        smallerNum = secondNum;
+        largerNum = firstNum;
+    }
+    for(let i=smallerNum; i<=largerNum; i++ && (smallerNum>=0) && (largerNum>=0) ){
+        value += i;
+    }
+    if (smallerNum<=0 || largerNum<=0 || typeof smallerNum !== 'number' || typeof largerNum !== 'number') {
+        return "ERROR";
+    }
+    else{
+    return value;
+    }
 };
 
 // Do not edit below this line
