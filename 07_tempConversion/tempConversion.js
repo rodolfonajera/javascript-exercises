7b16848d65011fa02bf4c7ad44af6531b78858a5
rodolfo najera <rodolfonajera401@gmail.com>
2024-02-22 00:30:15 -0800

    Complete exercise 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahrenheight) {
+  let celcius = 5 / 9 * (fahrenheight - 32);
+  celcius = Math.round(celcius * 10) / 10;
+  return celcius
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celcius) {
+  let fahrenheight = (celcius * 9 / 5) + 32;
+  fahrenheight = Math.round(fahrenheight * 10) / 10;
+  return fahrenheight;
 };
 
 // Do not edit below this line
