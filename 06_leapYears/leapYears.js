fa8bf06861f73abf0ba52a3d8f8804faee579a47
rodolfo najera <rodolfonajera401@gmail.com>
2024-02-22 00:00:01 -0800

    Complete exercise 06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,18 @@
-const leapYears = function() {
+const leapYears = function(num) {
+if (num % 4 === 0){
+    if(num % 100 === 0 && num % 400 === 0){
+        return true;
+    }
+   else if(num % 100 === 0){
+    return false;
+   }
+   else{
+    return true;
+   }
+}
+else{
+    return false;
+}
 
 };
 
